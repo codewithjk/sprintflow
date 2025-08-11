9640c7d3c57e7f8a8f13b718d6dd638d60c711ff
jeevan <jeevankumar8943@gmail.com>
2025-08-11 19:40:07 +0530

    FIX : search

--- apps/web/src/app/components/ui/navbar/index.tsx
+++ apps/web/src/app/components/ui/navbar/index.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search Bar */}
       <div className="flex items-center gap-8">
-        {!isSidebarCollapsed ? null : (
+        {/* {!isSidebarCollapsed ? null : (
           <button
             onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
           >
@@ -50,7 +50,7 @@ const Navbar = () => {
             type="search"
             placeholder="Search..."
           />
-        </div>
+        </div> */}
       </div>
 
       {/* Icons */}
