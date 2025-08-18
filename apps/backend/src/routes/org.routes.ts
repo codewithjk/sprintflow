4b76427370568d6543035a2c52fa8a6007dd15c4
jeevan <jeevankumar8943@gmail.com>
2025-08-18 20:49:27 +0530

    FIX : admin profile update error fixed

--- apps/backend/src/routes/org.routes.ts
+++ apps/backend/src/routes/org.routes.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/', searchOrganizationsController);
 router.post('/invite', authorizeRoles(AppUserRole.ORGANIZATION), inviteUserToOrgController);
 router.get('/members', authorizeRoles(AppUserRole.ORGANIZATION), getAllMembersController)
-router.put('/members/:id',authorizeRoles(AppUserRole.USER),updateMemberController)
+router.put('/members/:id',authorizeRoles(AppUserRole.USER,AppUserRole.SUPER_ADMIN),updateMemberController)
 router.put('/:id',authorizeRoles(AppUserRole.ORGANIZATION), updateOrganizationController);
 router.delete('/:id', authorizeRoles(AppUserRole.ORGANIZATION),deleteOrganizationController);
 router.get('/:id', getOrganizationController);
